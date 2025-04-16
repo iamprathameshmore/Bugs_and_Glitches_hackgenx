10596543cf08c7d864e633383cf046df264e7dde
iamprathameshmore <iamprathameshmore07@gmail.com>
2025-04-16 21:38:44 +0530

    add Application, IOT, Model

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+'use client';
+import { useEffect } from "react";
 
 export default function Home() {
+  // const handleGetData = async () => {
+  //   const res = await fetch('http://localhost:4213/{id}')
+  //   const data = await res.json()
+  // }
+  // useEffect(
+  //   () => {
+  //     handleGetData()
+  //   }
+  // )
   return (
     <div className="">
-      graphs
+      <div className="w-full p-5 flex justify-between items-center">
+        <p>Bugs & Glitches</p>
+        <p>Readings</p>
+
+      </div>
+
+
     </div>
   );
 }
--- frontend/src/app/readings/page.tsx
+++ frontend/src/app/readings/page.tsx
@@ -1,14 +1,14 @@
 export default function Page() {
     return <>
-   <div className="w-full h-full flex justify-center items-center p-5">
-   <div>
-   <div>
-        Readings - Bugs & Glitches
-    </div>
-    <div>
-        ...list of Readings
-    </div>
-   </div>
-   </div>
+        <div className="w-full h-full flex justify-center items-center p-5">
+            <div>
+                <div>
+                    Readings - Bugs & Glitches
+                </div>
+                <div>
+                    ...list of Readings
+                </div>
+            </div>
+        </div>
     </>
 }
